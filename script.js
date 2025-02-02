9293261a17a45a0526cf99fa63d020d13a49f3b0
elioficial <balalaika252@gmail.com>
2025-02-01 23:26:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function revealProfile() {
+    document.getElementById('profile-content').style.display = 'flex';
+    document.getElementById('reveal-button').style.display = 'none';
+    document.getElementById('background-music').play();
+}
